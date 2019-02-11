5283b5d9e4f864901839eee51f1e958a5cdccd86
pratimatamra60 <pratimatamra60@gmail.com>
2019-02-11 13:39:15 +0100

    core concept routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = {name:'wes',age:100, cool:true};
+  //res.send('Hey! It works!');
+   //res.json(wes);
+  //res.send(req.query.name); // localhost:7777/?name=wes
+  res.json(req.query); 
+  
 });
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+ /*  res.send('Hey! It works!');*/
+  res.send(reverse); // localhost:7777/reverse/:name=wes
+});
+
+
+
 
 module.exports = router;
