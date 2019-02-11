ce7442a10938e2f45d72245cfb1a3b6cf92d28b8
pratimatamra60 <pratimatamra60@gmail.com>
2019-02-11 17:16:46 +0100

    mvc pattern

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController')
 
 // Do work here
-router.get('/', (req, res) => {
-  const wes = {name:'wes',age:100, cool:true};
-  //res.send('Hey! It works!');
-  //res.json(wes);
-  //res.send(req.query.name); // localhost:7777/?name=wes
-  //res.json(req.query); 
-
-  /*To get information from route from database to the actual template render it*/
-   res.render('hello', {
-    name:'wes',
-    //dog:'barbie'
-    dog: req.query.name//pull in info from url
-  }); 
-
-  //res.render('hello');
-});
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
- /*  res.send('Hey! It works!');*/
-  res.send(reverse); // localhost:7777/reverse/:name=wes
-});
-
-
-
+router.get('/', storeController.homePage); 
 
 module.exports = router;
