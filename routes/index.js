fba15fdbf03a791a34d6e2996babfc0946ec80f3
pratimatamra60 <pratimatamra60@gmail.com>
2019-02-11 16:51:34 +0100

    templating in node

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,18 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const wes = {name:'wes',age:100, cool:true};
   //res.send('Hey! It works!');
-   //res.json(wes);
+  //res.json(wes);
   //res.send(req.query.name); // localhost:7777/?name=wes
-  res.json(req.query); 
-  
+  //res.json(req.query); 
+
+  /*To get information from route from database to the actual template render it*/
+   res.render('hello', {
+    name:'wes',
+    //dog:'barbie'
+    dog: req.query.name//pull in info from url
+  }); 
+
+  //res.render('hello');
 });
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
